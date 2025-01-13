165a0b7891468e0ef39d94763934eb922bfc0a90
mariblnd <marieblanchard7@laposte.net>
2025-01-13 13:36:10 +0100

    Exercice 1 : Grille de couleurs

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
+    background(0);
+    colorMode(HSL);
+
+    let widthRect = (width/2)/6;
+    let heightRect = (height/2)/4; 
+
+    let colors = [];
+
+    for(let i = 0; i<=5 ; i++){
+      let hue = random(0, 360);
+      let saturation = random(50, 100); 
+      let lightness = random(50, 80);
+
+      colors[i] = { hue, saturation, lightness };
+    }
+
+    let offsetX = (width - widthRect*6)/2;
+    let offsetY = (height - heightRect*4)/2;
+
+    for(let col = 0; col <= 3; col++){
+      for(let row = 0; row <=5; row++){
+
+        let color = random(colors);
+
+        noStroke();
+        fill(color.hue, color.saturation, color.lightness);
+        rect(offsetX+widthRect*row,offsetY+heightRect*col,widthRect,heightRect);
+        
+      }
+    }
+
   }
   
   function draw() {
-    background(220);
+
+    
+    
   }
